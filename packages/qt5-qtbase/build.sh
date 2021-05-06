e64e42b9229194d18c1b19778ade577089e176fa
Simeon Huang <hzwhuang@gmail.com>
2021-05-06 14:03:01 +0100

    [WIP] try to fix the segfault with xcb

--- packages/qt5-qtbase/build.sh
+++ packages/qt5-qtbase/build.sh
@@ -3,7 +3,7 @@ TERMUX_PKG_DESCRIPTION="A cross-platform application and UI framework"
 TERMUX_PKG_LICENSE="LGPL-3.0"
 TERMUX_PKG_MAINTAINER="Simeon Huang <symeon@librehat.com>"
 TERMUX_PKG_VERSION=5.15.2
-TERMUX_PKG_REVISION=5
+TERMUX_PKG_REVISION=7
 TERMUX_PKG_SRCURL="https://download.qt.io/official_releases/qt/5.15/${TERMUX_PKG_VERSION}/submodules/qtbase-everywhere-src-${TERMUX_PKG_VERSION}.tar.xz"
 TERMUX_PKG_SHA256=909fad2591ee367993a75d7e2ea50ad4db332f05e1c38dd7a5a274e156a4e0f8
 TERMUX_PKG_DEPENDS="dbus, harfbuzz, libandroid-shmem, libc++, libice, libicu, libjpeg-turbo, libpng, libsm, libuuid, libx11, libxcb, libxi, libxkbcommon, openssl, pcre2, ttf-dejavu, freetype, xcb-util-image, xcb-util-keysyms, xcb-util-renderutil, xcb-util-wm, zlib"
@@ -19,7 +19,6 @@ lib/qt/mkspecs/termux-cross
 # Replacing the old qt5-base packages
 TERMUX_PKG_REPLACES="qt5-base"
 TERMUX_PKG_BREAKS="qt5-x11extras, qt5-tools, qt5-declarative"
-TERMUX_PKG_CONFLICTS="qt5-base"
 
 TERMUX_PKG_QUICK_REBUILD=false
 
@@ -51,7 +50,8 @@ termux_step_configure () {
     "${TERMUX_PKG_SRCDIR}"/configure -v \
         -opensource \
         -confirm-license \
-        -release \
+        -debug \
+        -optimized-tools \
         -xplatform termux-cross \
         -shared \
         -no-rpath \
@@ -95,7 +95,8 @@ termux_step_configure () {
         -system-libpng \
         -system-libjpeg \
         -system-sqlite \
-        -sql-sqlite
+        -sql-sqlite \
+        -no-feature-systemsemaphore
 }
 
 termux_step_make() {
